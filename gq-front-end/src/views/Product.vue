6302572ea625a0afa5c5c9b9a43f2bc2a359b89a
Абдуллах Агаев <@itstep.loc>
2022-04-20 14:37:45 +0600

    first commit

---
+++ gq-front-end/src/views/Product.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1>Products</h1>
+    <div v-for="p in Products" :key="p.id">
+      <p>{{ p.id }}</p>
+      <p>{{ p.title }}</p>
+      <p>{{ p.description }}</p>
+      <img :src="'http://localhost:8055/assets/'+ p.image.id" alt=""/>
+    </div>
+  </div>
+</template>
+
+<script>
+import gql from "graphql-tag";
+export default {
+  apollo: {
+    Products: {
+      query: gql`
+        query products {
+      Products {
+        id
+        title
+        price
+        description
+        image {
+        id
+        }
+          }
+        }
+
+      `,
+    },
+  },
+  data() {
+    return {
+      limit: 10,
+    };
+  },
+  methods: {
+    move(id) {
+      this.$router.push("/products/" + id);
+    },
+  }
+};
+</script>
+
+<style scoped>
+.cart {
+  border: 1px solid black;
+  padding: 10px;
+}
+</style>
